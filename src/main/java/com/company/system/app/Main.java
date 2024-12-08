5ed601201b7bd5c957408a688cd2256460af5276
Ronald Seminario <ronaldseminario78@gmail.com>
2024-12-08 13:48:38 -0500

    Le implemente al CategoryBooks un JScrollPane para poder ver todas las opciones de libros cuando seleccionamos una categoria

--- src/main/java/com/company/system/app/Main.java
+++ src/main/java/com/company/system/app/Main.java
@@ -2,11 +2,14 @@ package com.company.system.app;
 
 import com.company.system.controller.InitialWindowListener;
 import com.company.system.view.InitialWindow;
+import com.company.system.view.Refactor;
 
 public class Main {
     public static void main(String[] args) {
-        InitialWindow frmInitial = new InitialWindow();
-        frmInitial.setVisible(true);
-        new InitialWindowListener(frmInitial);
+        //InitialWindow frmInitial = new InitialWindow();
+        //frmInitial.setVisible(true);
+        //new InitialWindowListener(frmInitial);
+        Refactor frm = new Refactor();
+        frm.setVisible(true);
     }
 }
