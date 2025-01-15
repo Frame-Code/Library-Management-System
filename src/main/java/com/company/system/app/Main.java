ccc86fb01d355237b06f4a0a6e13e87ecf992382
frame-code <moraisurcan@hotmail.com>
2025-01-14 22:19:55 -0500

    Cambio en el main

--- src/main/java/com/company/system/app/Main.java
+++ src/main/java/com/company/system/app/Main.java
@@ -1,10 +1,5 @@
-    package com.company.system.app;
+package com.company.system.app;
 
-import com.company.system.controller.InitialWindowListener;
-import com.company.system.controller.LibraryHomeListener;
-import com.company.system.service.UserService;
-import com.company.system.view.InitialWindow;
-import com.company.system.view.LibraryHome;
 
 /**
  * Clase principal de la aplicación. Contiene el método main que es el punto de entrada
@@ -13,24 +8,11 @@ import com.company.system.view.LibraryHome;
 public class Main {
     
     /**
-     * Método principal de la aplicación. Inicializa la ventana principal de la biblioteca y
-     * asigna el controlador correspondiente a la ventana.
+     * Método principal de la aplicación. Inicializa la ventana de eleccion del tipo de usuario para iniciar sesion
      * 
      * @param args Argumentos pasados a la aplicación (no utilizados en este caso).
      */
     public static void main(String[] args) {
-        // Inicialización comentada de la ventana inicial y su controlador.
-        //InitialWindow frmInitial = new InitialWindow();
-        //new InitialWindowListener(frmInitial, new UserService());
-        //frmInitial.setVisible(true);
-
-        // Inicializa la ventana principal de la biblioteca.
-        LibraryHome frm = new LibraryHome(null);
-
-        // Asocia el controlador LibraryHomeListener a la ventana de la biblioteca.
-        new LibraryHomeListener(frm, new UserService());
-
-        // Hace visible la ventana principal.
-        frm.setVisible(true);
+        
     }
 }
